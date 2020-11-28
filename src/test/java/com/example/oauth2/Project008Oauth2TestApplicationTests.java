6739e3965bb1681266ee789c8fb296f780862fad
NayeemBiswas <43644126+NayeemBiswas@users.noreply.github.com>
2020-11-28 17:20:21 +0600

    Add files via upload

---
+++ src/test/java/com/example/oauth2/Project008Oauth2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project008Oauth2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
